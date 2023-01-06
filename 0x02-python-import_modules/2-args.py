e9045b429e161fc9ef25ce71b53e48a73cb458c3
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-01-06 13:16:04 +0000

    added line 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argv_len = (len(argv) - 1)
+    argv_len = (len(argv) - 1)
 
-if argv_len == 0:
-    print(f"{argv_len} arguments.")
-elif argv_len == 1:
-    print(f"{argv_len} argument:")
-    print(argv[argv_len])
-else:
-    print(f"{argv_len} arguments:")
-    for i in range(argv_len):
-        print(f"{i+1}: {argv[i+1]}")
+    if argv_len <= 0:
+        print(f"{argv_len} arguments.")
+    elif argv_len == 1:
+        print(f"{argv_len} argument:")
+        print(argv[argv_len])
+    else:
+        print(f"{argv_len} arguments:")
+        for i in range(argv_len):
+            print(f"{i+1}: {argv[i+1]}")
