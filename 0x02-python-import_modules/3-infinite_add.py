c8dc2b18058ddfcc2e53cc8189dfd24ee095d22a
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-01-06 13:20:58 +0000

    added line 2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argv_len = (len(argv) - 1)
+    argv_len = (len(argv) - 1)
 
-if argv_len == 0:
-    print(argv_len)
-else:
-    count = 0
-    for i in range(1, argv_len+1):
-        count += int(argv[i])
-    print(count)
+    if argv_len == 0:
+        print(argv_len)
+    else:
+        count = 0
+        for i in range(1, argv_len+1):
+            count += int(argv[i])
+        print(count)
