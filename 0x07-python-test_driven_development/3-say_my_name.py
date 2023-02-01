11bca906072c2e334a8143494edb484cf9cab88a
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-02-01 07:20:44 +0000

    pep style fixes

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,5 +12,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    
     print(f"My name is {first_name} {last_name}")
