61976942a4c922fd3d12d59ca5833dd810fc50ad
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-02-01 08:25:35 +0000

    pep style fixes

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,6 +14,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if type(size) == float and size < 0:
         raise TypeError("size must be an integer")
-    
     for i in range(size):
         print("#" * size)
