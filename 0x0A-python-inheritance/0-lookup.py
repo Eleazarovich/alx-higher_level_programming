c56648b649a95fe59e4d702e75c848e3d71645ca
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-02-06 16:51:06 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-lookup.py module
+
+the module contains one function, def lookup(obj)
+"""
+
+
+def lookup(obj):
+    """returns the a list object"""
+    return dir(obj)
