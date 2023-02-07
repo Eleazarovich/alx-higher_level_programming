64c74547c48a5728e71e82c64c6d02412dc5523e
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-02-07 05:50:35 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py module
+
+the module contains one class, based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def __init__(self):
+        """new instance of BaseGeometry"""
+        pass
+
+    def area(self):
+        """raises an exception if area is not implemeted"""
+        raise Exception("area() is not implemented")
