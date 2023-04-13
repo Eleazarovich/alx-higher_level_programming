36d7c39c2a481d4247d0931d7097945c94c352d6
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-04-13 18:38:59 +0000

    rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // empty object
+    }
+  }
+}
+
+module.exports = Rectangle;
