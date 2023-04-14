fabc4f523abd272a3560f4059f5ad0c7d1c6a928
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-04-14 09:43:15 +0000

    esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newArr = [];
+
+  for (let i = (list.length - 1); i >= 0; i--) {
+    newArr.push(list[i]);
+  }
+  return newArr;
+};
