ea0212ed715edcfb9424eac596c89c1510674738
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-05-18 13:17:04 +0200

    what's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this script fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
